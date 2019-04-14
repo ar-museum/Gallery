8039f52ab6b9a419c4d1374deb69fe94cbb68c04
adriandlupu <48430145+adriandlupu@users.noreply.github.com>
2019-04-14 23:14:02 +0300

    .

---
+++ Gallery/Assets/Scripts/ChooseScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseScene : MonoBehaviour
+{
+    public void changeIt(int i)
+    { SceneManager.LoadScene(i); }
+}
