3fb565c53556229490f9894f9a469eed1dfc916b
adriandlupu <48430145+adriandlupu@users.noreply.github.com>
2019-04-15 09:28:14 +0300

    .

--- Gallery/Assets/Scripts/ChooseScene.cs
+++ Gallery/Assets/Scripts/ChooseScene.cs
@@ -2,9 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Text;
 
 public class ChooseScene : MonoBehaviour
 {
     public void changeIt(int i)
-    { SceneManager.LoadScene(i); }
+    {
+        SceneManager.LoadScene(i);
+        FileStream fs = File.Create("Assets/nameeee.txt");
+        string path = "Assets/nameeee.txt";
+        fs.Dispose();
+        using (StreamWriter writer = new StreamWriter(path,true))
+        {
+            writer.WriteLine("op1");
+            writer.WriteLine("cevaaa");
+        }
+
+    }
 }
---
+++ Gallery/Assets/Scripts/ChooseScene2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Text;
+
+public class ChooseScene2 : MonoBehaviour
+{
+    public void changeIt2(int i)
+    {
+        SceneManager.LoadScene(i);
+        FileStream fs = File.Create("Assets/nameeee.txt");
+        string path = "Assets/nameeee.txt";
+        fs.Dispose();
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine("op3");
+            writer.WriteLine("cevaaa");
+        }
+
+    }
+}
