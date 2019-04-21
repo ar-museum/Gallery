217cfe09736722c121808798863026f064c8ca10
LaviniaDisca <48357956+LaviniaDisca@users.noreply.github.com>
2019-04-21 13:41:38 +0300

    Play audio and back to author.

---
+++ Gallery/Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+ 
+    private AudioSource clipSource;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        clipSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clipSource.PlayDelayed(0);
+        }
+    }
+}
