3515fe46bc955821b1647079fba61001e1d4ec83
LaviniaDisca <48357956+LaviniaDisca@users.noreply.github.com>
2019-04-21 20:02:08 +0300

    PlayButton

--- Gallery/Assets/Scripts/AudioPlay.cs
+++ Gallery/Assets/Scripts/AudioPlay.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class AudioPlay : MonoBehaviour
 {
  
-    private AudioSource clipSource;
-     
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource clipSource;
+    public AudioClip click;
+
+    public void clickSound()
     {
-        clipSource = GetComponent<AudioSource>();
+        clipSource.PlayOneShot(click);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            clipSource.PlayDelayed(0);
-        }
-    }
+    // Start is called before the first frame update
+    /* void Start()
+     {
+         clipSource = GetComponent<AudioSource>();
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+         if (Input.GetMouseButtonDown(0))
+         {
+             clipSource.PlayDelayed(0);
+         }
+     } */
 }
